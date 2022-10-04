35341450d6f10f3c66677fb16eeb1b3e16eaa2ad
MaximilianHausen <Max.Ha@directbox.com>
2022-10-04 14:58:04 +0200

    Split cookies

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,9 @@
 use axum::{Extension, Router};
 use axum::http::StatusCode;
 use axum::routing::post;
-use axum_extra::extract::cookie::{Cookie, Key};
+use axum_extra::extract::cookie::{Cookie, Expiration, Key};
 use axum_extra::extract::PrivateCookieJar;
+use time::{Duration, OffsetDateTime};
 
 #[tokio::main]
 async fn main() {
@@ -21,18 +22,23 @@ async fn main() {
 }
 
 async fn login(mut jar: PrivateCookieJar, body: String) -> Result<PrivateCookieJar, StatusCode> {
-    match homeworker_api_client::auth::exchange_token(std::env::var("CLIENT_ID").unwrap(), std::env::var("CLIENT_SECRET").unwrap(), body).await {
+    match homeworker::auth::exchange_token(std::env::var("CLIENT_ID").unwrap(), std::env::var("CLIENT_SECRET").unwrap(), body).await {
         Ok(response) => {
-            jar = jar.add(Cookie::build("proxy-session", format!("{}|{}", response.access_token, response.refresh_token)).http_only(true).secure(true).finish());
+            jar = jar.add(Cookie::build("access-token", response.access_token)
+                .http_only(true).secure(true)
+                .expires(Expiration::DateTime(OffsetDateTime::now_utc() + Duration::days(1))).finish());
+            jar = jar.add(Cookie::build("refresh-token", response.refresh_token)
+                .http_only(true).secure(true)
+                .expires(Expiration::from(OffsetDateTime::now_utc() + Duration::days(729))).finish());
             Ok(jar)
         }
         Err(error) => match error {
-            homeworker_api_client::Error::RequestError(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
-            homeworker_api_client::Error::ApiError(err) => Err(StatusCode::from_u16(err.code).unwrap()),
+            homeworker::Error::RequestError(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+            homeworker::Error::ApiError(err) => Err(StatusCode::from_u16(err.code).unwrap()),
         },
     }
 }
 
 async fn logout(jar: PrivateCookieJar) -> PrivateCookieJar {
-    jar.remove(Cookie::named("proxy-session"))
+    jar.remove(Cookie::named("access-token")).remove(Cookie::named("refresh-token"))
 }
