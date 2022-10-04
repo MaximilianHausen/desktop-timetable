5a16bf54a34116bf0d0d12d33ae11eda91a33ea6
MaximilianHausen <Max.Ha@directbox.com>
2022-10-04 14:59:19 +0200

    Backend proxy

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
 use axum::{Extension, Router};
+use axum::extract::Path;
 use axum::http::StatusCode;
-use axum::routing::post;
+use axum::routing::{get, post};
 use axum_extra::extract::cookie::{Cookie, Expiration, Key};
 use axum_extra::extract::PrivateCookieJar;
 use time::{Duration, OffsetDateTime};
@@ -12,6 +13,7 @@ async fn main() {
     let app = Router::new()
         .route("/login", post(login))
         .route("/logout", post(logout))
+        .route("/api/v2/*path", get(proxy_get).post(proxy_post).delete(proxy_delete))
         .layer(Extension(key));
 
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -41,4 +43,54 @@ async fn login(mut jar: PrivateCookieJar, body: String) -> Result<PrivateCookieJ
 
 async fn logout(jar: PrivateCookieJar) -> PrivateCookieJar {
     jar.remove(Cookie::named("access-token")).remove(Cookie::named("refresh-token"))
+}
+
+async fn proxy_get(jar: PrivateCookieJar, Path(path): Path<String>) -> Result<(StatusCode, String), (StatusCode, String)> {
+    let access_token = match jar.get("access-token") {
+        Some(cookie) => cookie.value().to_owned(),
+        None => return Err((StatusCode::UNAUTHORIZED, "".to_owned())),
+    };
+
+    let response = match reqwest::Client::new().get("https://homeworker.li/api/v2".to_owned() + &path)
+        .header("Authorization", "Bearer ".to_owned() + &access_token)
+        .send().await {
+        Ok(response) => response,
+        Err(_) => return Err((StatusCode::INTERNAL_SERVER_ERROR, "".to_owned())),
+    };
+
+    Ok((response.status(), response.text().await.unwrap()))
+}
+
+async fn proxy_post(jar: PrivateCookieJar, Path(path): Path<String>, body: String) -> Result<(StatusCode, String), (StatusCode, String)> {
+    let access_token = match jar.get("access-token") {
+        Some(cookie) => cookie.value().to_owned(),
+        None => return Err((StatusCode::UNAUTHORIZED, "".to_owned())),
+    };
+
+    let response = match reqwest::Client::new().post("https://homeworker.li/api/v2".to_owned() + &path)
+        .header("Authorization", "Bearer ".to_owned() + &access_token)
+        .body(body)
+        .send().await {
+        Ok(response) => response,
+        Err(_) => return Err((StatusCode::INTERNAL_SERVER_ERROR, "".to_owned())),
+    };
+
+    Ok((response.status(), response.text().await.unwrap()))
+}
+
+async fn proxy_delete(jar: PrivateCookieJar, Path(path): Path<String>, body: String) -> Result<(StatusCode, String), (StatusCode, String)> {
+    let access_token = match jar.get("access-token") {
+        Some(cookie) => cookie.value().to_owned(),
+        None => return Err((StatusCode::UNAUTHORIZED, "".to_owned())),
+    };
+
+    let response = match reqwest::Client::new().delete("https://homeworker.li/api/v2".to_owned() + &path)
+        .header("Authorization", "Bearer ".to_owned() + &access_token)
+        .body(body)
+        .send().await {
+        Ok(response) => response,
+        Err(_) => return Err((StatusCode::INTERNAL_SERVER_ERROR, "".to_owned())),
+    };
+
+    Ok((response.status(), response.text().await.unwrap()))
 }
